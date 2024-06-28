9b9085fb68c1048acb1d9abadb1206a9c089ced0
Duenas0002 <136118495+Duenas0002@users.noreply.github.com>
2024-06-27 20:40:57 -0500

    Test 2
    Old file #2

---
+++ Test 2.py
@@ -0,0 +1,24 @@
+import random
+
+print('Ask your question')
+
+NU = input()
+num = random.randint(1,9)
+if num==9:
+  print('Yes')
+elif num == 8:
+  print('It is decidedly so.')
+elif num == 7:
+  print('Without a doubt.')
+elif num == 6:
+  print('Reply hazy, try again.')
+elif num == 5:
+  print('Ask again later.')
+elif num == 4: 
+  print('Better not tell you now.')
+elif num == 3:
+  print('My sources say no.')
+elif num == 2:
+  print('Outlook not so good.')
+else:
+  print('Very doubtful.')
