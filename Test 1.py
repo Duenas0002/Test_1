11165762956c048f98223e9cb0a5fe45396671af
Duenas0002 <136118495+Duenas0002@users.noreply.github.com>
2024-06-27 20:11:57 -0500

    Test 1
    Old file#1

---
+++ Test 1.py
@@ -0,0 +1,10 @@
+print("Howdy lil bro! How are ya?, So tell me your height in centimeters not in inches")
+Height = int(input())
+print('Good job lil bro, now I need you to tell me how many credits you have in your account')
+Credits = int(input())
+if Height>137 and Credits>=10:
+  print('You might pass')
+else:
+  print("Bro you're either a dwarf or broke, get outta here")
+
+  
